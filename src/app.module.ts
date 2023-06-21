9e8dd0616fca86620406fd961c5e6017949e9b23
Jovan Drobnjak <jovan.drobnjak@vega.local>
2023-06-21 13:43:09 +0200

    Start working on microservice architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { Coffee } from './coffee/coffee.entity';
 import { CoffeeModule } from './coffee/coffee.module';
 import { BarmenModule } from './barmen/barmen.module';
 import { OrderModule } from './order/order.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -17,7 +18,10 @@ import { OrderModule } from './order/order.module';
       database: 'coffee_shop',
       entities: [Coffee],
       synchronize: true
-}),CoffeeModule,BarmenModule,OrderModule],
+}),CoffeeModule,BarmenModule,OrderModule, ClientsModule.register([
+  {name : 'BARISTA',
+  transport: Transport.TCP}
+])],
   controllers: [AppController],
   providers: [AppService],
 })
